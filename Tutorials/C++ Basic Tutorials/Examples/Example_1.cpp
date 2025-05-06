81f79b8acf36d10e53a38b18dada03852c3eaaaf
JorgeAlejo <jmejiaholguin@gmail.com>
2025-05-05 21:52:22 -0500

    Example 1

---
+++ Tutorials/C++ Basic Tutorials/Examples/Example_1.cpp
@@ -0,0 +1,12 @@
+/**Example 1**
+
+Create a variable called x of type int and assign it the value 15:*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 15;
+    cout << x;
+    return 0;
+}
