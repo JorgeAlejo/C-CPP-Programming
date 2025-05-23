9b9f84034b922a929516f4833a14b399e4a19d47
JorgeAlejo <jmejiaholguin@gmail.com>
2025-05-23 11:58:17 -0500

    Create Example_4.cpp
    Example 4:  regarding Registers... Registers are depreciated

---
+++ Tutorials/C++ Basic Tutorials/Examples/Example_4.cpp
@@ -0,0 +1,16 @@
+/*Example 4: regarding registers*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y=20, z=30;
+    x=y+z;
+    cout<<"Add = "<<x<<endl;
+    return 0;
+}
+
+ /*
+ ### Output ###
+ Add=50
+ */
