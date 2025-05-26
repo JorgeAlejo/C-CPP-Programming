fae1b0874517d977feeadafee9c8d7f0023aa509
JorgeAlejo <jmejiaholguin@gmail.com>
2025-05-26 10:30:53 -0500

    Create Example_6.cpp
    Example 6 Regarding Aritmetic Operator

---
+++ Tutorials/C++ Basic Tutorials/Examples/Example_6.cpp
@@ -0,0 +1,23 @@
+/*Example 6 Regarding Aritmetic Operator*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=5, b=3;
+    cout<<(a+b)<<"\n";
+    cout<<(a-b)<<"\n";
+    cout<<(a*b)<<"\n";
+    cout<<(a/b)<<"\n";
+    cout<<(a%b)<<"\n"; //Modulus hold the remain
+    return 0;
+}
+
+ /*
+ ### Output ###
+ 8
+ 2
+ 15
+ 1
+ 2
+ */
