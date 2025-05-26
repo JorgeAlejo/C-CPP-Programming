897d039fd07d9f4b3b89635745830d6364f1cbb3
JorgeAlejo <jmejiaholguin@gmail.com>
2025-05-26 11:02:50 -0500

    Create Example_10.cpp
    Example 10: regarding conditional operators

---
+++ Tutorials/C++ Basic Tutorials/Examples/Example_10.cpp
@@ -0,0 +1,10 @@
+// Example 10: regarding conditional operators
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=10, b=20;
+    a>b?cout<<"a is greater than b":cout<<"b is greater than a";
+    return 0;
+}
