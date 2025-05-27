0d6b570e40b97dee6c81d31e39e6d1813eddbcea
JorgeAlejo <jmejiaholguin@gmail.com>
2025-05-27 10:27:40 -0500

    Create Example_11.cpp
    Example 11: regarding if statement

---
+++ Tutorials/C++ Basic Tutorials/Examples/Example_11.cpp
@@ -0,0 +1,13 @@
+// Example 11: regarding if statement
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    int x=50,y=20;
+    if(x>y){
+        cout<<"x is greater than y";
+    }
+    return 0;
+}
